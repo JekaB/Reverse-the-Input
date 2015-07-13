9ced9bcde9be4dfc9c994f4403e9a3ed54d2681b
Evgeiy_B <bocharovevgen@gmail.com>
2015-07-13 18:12:43 +0300

    new origin commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_the_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lst = new List<string>();
+            Console.WriteLine("Когда надоеcт нажмине END");
+            bool quit = true;
+
+            string s = "";
+            while (quit)
+            {
+                s = Console.ReadLine();
+                if (s != "END")
+                {
+                    lst.Add(s);
+
+                }
+                else
+                {
+                    quit = false;
+                }
+            }
+
+            lst.Reverse();
+            foreach (var item in lst)
+            {
+                Console.WriteLine("-> {0}", item);
+            }
+            Console.WriteLine("END");
+            Console.ReadKey();
+        }
+    }
+}
